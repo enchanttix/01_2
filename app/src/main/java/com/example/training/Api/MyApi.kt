bb9e402bf7a6c3c86caca439ca354d85dac1410a
Smerdova Liza <smerdova_vz@mail.ru>
2024-01-16 00:58:12 +0300

    first commit

--- app/src/main/java/com/example/training/Api/MyApi.kt
+++ app/src/main/java/com/example/training/Api/MyApi.kt
@@ -3,8 +3,14 @@ package com.example.training.Api
 import com.example.training.Model.Films
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface MyApi {
     @GET("api/Movie")
     fun getsMovie(): Call<List<Films>>
+    @POST("api/SendCode")
+    fun postEmail(@Header("User-email") email: String): Call<String>
+    @POST("api/SignIn")
+    fun signIn(@Header("User-email") email: String, @Header("User-code") code: String): Call<String>
 }
